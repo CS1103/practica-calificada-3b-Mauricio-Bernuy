180c4d601cf8f9e7fe2ad7c186bbd4413a47df38
Mauricio-Bernuy <54604331+Mauricio-Bernuy@users.noreply.github.com>
2019-11-18 00:39:39 -0500

    arrange.h

--- arrange.h
+++ arrange.h
@@ -6,11 +6,20 @@
 #include <string>
 #include <sstream>
 #include <unordered_map>
+#include <algorithm>
 
 #include "stovals.h"
 
+using namespace std;
+
+auto compareInput = [](std::pair<int, string> const& a, std::pair<int, string> const& b){
+	auto i1 = stovals(a.second);
+	auto i2 = stovals(b.second);
+	
+	return i1[0] != i2[0] ? i1[0] < i2[0] : i1[1] != i2[1] ? i1[1] < i2[1] : i1[2] != i2[2] ? i1[2] < i2[2] : i1[3] < i1[3];
+};
+
 void arrange(unordered_map<int, string> input) {
-	for (auto iter = input.begin(); iter != input.end(); iter++) {
-		stovals(iter->second);
-	}
+
+	sort(input.begin(), input.end(), compareInput);
 }
